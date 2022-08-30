a8e19609103a5c48d6fb77b58a9ca201b23c8b66
hilary81 <hilary_yong@yahoo.com>
2022-08-30 22:23:40 +1200

    first push

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import BingSearch from './components/BingSearch';
+
+function App() {
+  return (
+    <div className='App' >
+      <BingSearch />
+    </div>
+  );
+}
+
+export default App;
