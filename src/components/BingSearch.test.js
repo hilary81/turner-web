2f41527927f30d935f094a3ccaa3c15d994fd54b
hilary81 <hilary_yong@yahoo.com>
2022-08-31 14:29:47 +1200

    adding some fail test to be fix

--- src/components/BingSearch.test.js
+++ src/components/BingSearch.test.js
@@ -2,6 +2,6 @@ const punctremoved= require('./BingSearch');
 
 describe("Punctuation check", () =>{
     test("Hello*World should return Hello World", () =>{
-          expect(punctremoved("Hello,World")).toBe("Hello World")
+          expect(punctremoved("Hello, World")).toBe("Hello World")
     });
 })
